2c2fc5a38f5e3887c04ea0f5760a7bdfec352d32
souvikmaji <souvikmaji94@gmail.com>
2018-06-09 08:55:08 +0530

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,31 @@
 var express = require("express");
 var router = express.Router();
 var vision = require("@google-cloud/vision");
+var multer = require("multer");
 
 /* GET home page. */
-router
-  .get("/", function(req, res, next) {
-    res.render("index", { title: "Express" });
-  })
-  .post("/", function(req, res, next) {
-    var client = new vision.ImageAnnotatorClient();
-    var img = req.file.buffer;
-    client
-      .labelDetection(img)
-      .then(results => {
-        res.render("index", {
-          title: "express",
-          imgSrc: img.toString("base64"),
-          labels: results[0].labelAnnotations
-        });
-      })
-      .catch(err => {
-        res.render("index", { title: "Express" });
-        console.error("ERROR:", err);
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* POST to home page. */
+router.post("/", multer().single("flower"), function(req, res, next) {
+  var client = new vision.ImageAnnotatorClient();
+  var img = req.file.buffer;
+  client
+    .labelDetection(img)
+    .then(results => {
+      var labels = results[0].labelAnnotations;
+      res.render("index", {
+        title: "express",
+        imgSrc: img.toString("base64"),
+        labels: labels
       });
-  });
+    })
+    .catch(err => {
+      res.render("index", { title: "Express" });
+      console.error("ERROR:", err);
+    });
+});
 
 module.exports = router;
